3c9514351fe231699af1e81b00adbbc5e3ae1e0c
Marcelo Ferreira <118019156+Marcelofsilva26@users.noreply.github.com>
2023-11-07 01:23:07 -0300

    Create Herança1.py

---
+++ Herança1.py
@@ -0,0 +1,13 @@
+from Biblioteca import *
+
+gato = Gato('mingau', 'marrom')
+gato.emitirSom()
+
+vaca = Vaca('leitosa', 'branca')
+vaca.emitirSom()
+
+coelho = Coelho('pernalonga', 'branco')
+coelho.emitirSom()
+
+cachorro = Cachorro('tótó', 'preto')
+cachorro.emitirSom()
